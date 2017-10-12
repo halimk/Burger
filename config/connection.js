9d5d8f4e451c6b3f2948158461af2cee99dc5e08
halimk <kalhlmk@aol.com>
2017-10-12 12:31:37 -0400

    burger

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
-var mysql = require('mysql');
-
 var source = {
 	localhost: {
 		host: 'localhost',
 		user: 'root',
 		password: 'duke4566',
 		database: 'burgers_db'
+	},
+	jaws: {
+		port: 3306,
+        host: 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'y9cvwb8kybj2au7s',
+        password: "kidhs8m3rzm7l2y8",
+        database: "i69fnoqn0j5327jg" 
 	}
 };
-
-var connection = mysql.createConnection(source.localhost);
+var connection = mysql.createConnection(source.jaws);
 
 connection.connect(function (err) {
 	if (err) {
