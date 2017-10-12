a95d72865a149d6de14fbfa7ee989232600814d6
halimk <kalhlmk@aol.com>
2017-10-12 13:03:42 -0400

    burger

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,44 @@
-var source = {
-	localhost: {
-		host: 'localhost',
-		user: 'root',
-		password: 'duke4566',
-		database: 'burgers_db'
-	},
-	
-};
-var connection = mysql.createConnection(source.localhost);
+// var source = {
+// 	localhost: {
+// 		host: 'localhost',
+// 		user: 'root',
+// 		password: 'duke4566',
+// 		database: 'burgers_db'
+// 	},
+// 	jaws: {
+// 		port: 3306,
+//         host: 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//         user: 'y9cvwb8kybj2au7s',
+//         password: "kidhs8m3rzm7l2y8",
+//         database: "i69fnoqn0j5327jg" 
+// 	}
+// };
+// var connection = mysql.createConnection(source.localhost);
 
-connection.connect(function (err) {
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadId);
+// connection.connect(function (err) {
+// 	if (err) {
+// 		console.error('error connecting: ' + err.stack);
+// 		return;
+// 	}
+// 	console.log('connected as id ' + connection.threadId);
+// });
+
+// module.exports = connection;
+
+var mysql = require('mysql');
+var connection;
+    
+connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: "duke4566",
+    database: 'localhost'
 });
-
+connection.connect(function(err){
+    if(err){
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
 module.exports = connection;
